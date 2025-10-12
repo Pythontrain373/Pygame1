07d3be42ceef6063e5c7a40b1548c3f352215d68
Pythontrain373 <atikashwani@gmail.com>
2025-10-12 08:39:37 +0800

    class43project.py

--- class43project.py
+++ class43project.py
@@ -1,3 +1,10 @@
+import turtle
+turtle.Screen().bgcolor("green")
+turtle.Screen().setup(600,600)
+fonttype='normal','bold','italic'
+align='left','center','right'
+hi=turtle.Turtle()
+hi.speed(500)
 import tkinter as tk
 import random
 root=tk.Tk()
@@ -6,12 +13,15 @@ root.geometry("400x400")
 def checkwinner(userchoice):
     computerchoice=random.choice(['rock','paper','scissors'])
     if userchoice==computerchoice:
-        result="It's a tie!"
-    elif(userchoice=='rock'and computerchoice=='scissors')or(userchoice=='paper'and computerchoice=='rock')or(userchoice=='scissors'and computerchoice=='paper'):
-        result="You win!"
+        hi.clear()
+        hi.write("It is a tie",font=('Courier',30,'italic'))
+    elif (userchoice=='rock'and computerchoice=='scissors')or(userchoice=='paper'and computerchoice=='rock')or(userchoice=='scissors'and computerchoice=='paper'):
+        hi.clear()
+        hi.write("You win",font=('Courier',30,'italic'))
     else:
-        result="Computer wins!"
-    resultlabel.config(text=f"Computer chose: {computerchoice}\n{result}")
+        hi.clear()
+        hi.write("Computer wins",font=('Courier',30,'italic'))
+    resultlabel.config(text=f"Computer chose: {computerchoice}")
 rockbutton=tk.Button(root,text="Rock",command=lambda:checkwinner('rock'))
 rockbutton.pack(pady=10)
 paperbutton=tk.Button(root,text="Paper",command=lambda:checkwinner('paper'))
@@ -20,4 +30,5 @@ scissorsbutton=tk.Button(root,text="Scissors",command=lambda:checkwinner('scisso
 scissorsbutton.pack(pady=10)
 resultlabel=tk.Label(root,text="")
 resultlabel.pack(pady=20)
+turtle.done()
 root.mainloop()
